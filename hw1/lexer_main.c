01872da86edcda9e00bfb6a502ac12aedab163de
Linh Bui <linh@Linh-Buis-MacBook-Pro.local>
2012-02-06 23:55:25 -0500

    added reserved words, string and char const, getting segfault on char const

--- hw1/lexer_main.c
+++ hw1/lexer_main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "token_constants.h"
 
@@ -11,7 +12,7 @@ extern int yylineno;
 extern void *yylval;
 
 void initialize_token_names();
-void get_token_name(char *name, int token_number);
+// void get_token_name(char *name, int token_number);
 
 int main(int argc, char **argv) {
 	FILE *input, *output;
@@ -26,11 +27,23 @@ int main(int argc, char **argv) {
 	for (token = yylex(); token != 0; token = yylex()) {
 		// name = malloc(strlen(token_names[token]) + 1);
 		// strcpy(name, token_names[token]);
-		printf("%s\t", token_names[token]);
-		if (token == NUM) {
+		printf("Line number:\t%d\tToken text:\t%s\tToken type:\t%s\t", yylineno, yytext, token_names[token]);
+		if (token == NUM_CONST) {
 			number = (struct number*)yylval;
-			printf("%ld", number->value);
+			printf("Token value:\t%ld\t", number->value);
+			free(yylval);
 		}
+		if (token == STRING_CONST || token == CHAR_CONST) {
+			char *value = (char *)yylval;
+			printf("Token value:\t%s\t", value);
+		}
+		if (token == CHAR_CONST) {
+			char value = (char)yylval;
+			printf("Token value:\t%s\t", value);
+		}
+		printf("\n");
+		if (yylval != 0)
+			free(yylval);
 	}
 	if (output != stdout)
 		fclose(output);
@@ -40,7 +53,26 @@ int main(int argc, char **argv) {
 }
 
 void initialize_token_names() {
-	ADD_TOKEN_NAME(token_names, NUM);
+	ADD_TOKEN_NAME(token_names, DO);
+	ADD_TOKEN_NAME(token_names, FOR);
+	ADD_TOKEN_NAME(token_names, RETURN);
+	ADD_TOKEN_NAME(token_names, BREAK);
+	ADD_TOKEN_NAME(token_names, SHORT);
+	ADD_TOKEN_NAME(token_names, ELSE);
+	ADD_TOKEN_NAME(token_names, GOTO);
+	ADD_TOKEN_NAME(token_names, SIGNED);
+	ADD_TOKEN_NAME(token_names, UNSIGNED);
+	ADD_TOKEN_NAME(token_names, CHAR);
+	ADD_TOKEN_NAME(token_names, IF);
+	ADD_TOKEN_NAME(token_names, VOID);
+	ADD_TOKEN_NAME(token_names, INT);
+	ADD_TOKEN_NAME(token_names, CONTINUE);
+	ADD_TOKEN_NAME(token_names, LONG);
+	ADD_TOKEN_NAME(token_names, WHILE);
+	
+	ADD_TOKEN_NAME(token_names, NUM_CONST);
+	ADD_TOKEN_NAME(token_names, STRING_CONST);
+	ADD_TOKEN_NAME(token_names, CHAR_CONST);
 }
 
 // void get_token_name(char *name, int token_number) {
--- hw1/token_constants.h
+++ hw1/token_constants.h
@@ -1,4 +1,26 @@
-#define NUM 2
+// reserved words
+#define	DO	1
+#define	FOR	2
+#define	RETURN	3
+#define	BREAK	4
+#define	SHORT	5
+#define	ELSE	6
+#define	GOTO	7
+#define	SIGNED	8
+#define	UNSIGNED	9
+#define	CHAR	10
+#define	IF	11
+#define	VOID	12
+#define	INT	13
+#define	CONTINUE	14
+#define	LONG	15
+#define	WHILE	16
+
+//constants
+#define NUM_CONST 17
+#define STRING_CONST 18
+#define CHAR_CONST 19
+
 #define ADD_TOKEN_NAME(TOKEN_NAMES, TOKEN) TOKEN_NAMES[TOKEN] = #TOKEN
 const char *token_names[100];
 
