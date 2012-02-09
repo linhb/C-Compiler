0804f2aeb3af384b3062f1ee08051f2582084685
Linh Bui <linh@Linh-Buis-MacBook-Pro.local>
2012-02-08 20:40:26 -0500

    mostly working

--- hw1/lexer_main.c
+++ hw1/lexer_main.c
@@ -12,38 +12,73 @@ extern int yylineno;
 extern void *yylval;
 
 void initialize_token_names();
-// void get_token_name(char *name, int token_number);
+void initialize_operator_names();
+void print_value(void *value);
 
 int main(int argc, char **argv) {
 	FILE *input, *output;
 	input = stdin;
 	output = stdout;
 	initialize_token_names();
+	initialize_operator_names();
 	int token;
-	// char *name;
-	struct number *number;
-	char *id;
 	
 	for (token = yylex(); token != 0; token = yylex()) {
-		// name = malloc(strlen(token_names[token]) + 1);
-		// strcpy(name, token_names[token]);
-		printf("Line number:\t%d\tToken text:\t%s\tToken type:\t%s\t", yylineno, yytext, token_names[token]);
-		if (token == NUM_CONST) {
-			number = (struct number*)yylval;
-			printf("Token value:\t%ld\t", number->value);
-			free(yylval);
+		if (token > 0) {
+			printf("Line number:\t%d\tToken text:\t%s\tToken type:\t%s\t", yylineno, yytext, token_names[token]);
+			switch (token) {
+				struct number *number;
+				case NUM_CONST: 
+					number = (struct number *)yylval;
+					printf("Token value:\t%ld\t", number->value);
+					break;
+				case STRING_CONST:
+				case CHAR_CONST:
+				case IDENTIFIER:
+					print_value(yylval);		
+					break;
+				case LOGICAL_OP:
+				case BITWISE_OP:
+				case COMPARISON_OP:
+				case ASSIGNMENT_OP:
+				case ARITHMETIC_OP:
+				case BITSHIFT_OP:
+				
+				case LEFT_PAREN:
+				case RIGHT_PAREN:
+				case LEFT_BRACKET:
+				case RIGHT_BRACKET:
+				case LEFT_CURLY_BRACE:
+				case RIGHT_CURLY_BRACE:
+				case COMMA:
+				case SEMICOLON:
+				case COLON:
+					print_value(yylval);		
+					break;
+				case DO:
+				case FOR:
+				case RETURN:
+				case BREAK:
+				case SHORT:
+				case ELSE:
+				case GOTO:
+				case SIGNED:
+				case UNSIGNED:
+				case CHAR:
+				case IF:
+				case VOID:
+				case INT:
+				case CONTINUE:
+				case LONG:
+				case WHILE:
+					break;
+			}
 		}
-		if (token == STRING_CONST || token == CHAR_CONST) {
-			char *value = (char *)yylval;
-			printf("Token value:\t%s\t", value);
-		}
-		if (token == CHAR_CONST) {
-			char value = (char)yylval;
-			printf("Token value:\t%s\t", value);
+		else {
+			printf("ERROR");
 		}
 		printf("\n");
-		if (yylval != 0)
-			free(yylval);
+		yylineno++;
 	}
 	if (output != stdout)
 		fclose(output);
@@ -73,9 +108,67 @@ void initialize_token_names() {
 	ADD_TOKEN_NAME(token_names, NUM_CONST);
 	ADD_TOKEN_NAME(token_names, STRING_CONST);
 	ADD_TOKEN_NAME(token_names, CHAR_CONST);
+	
+	ADD_TOKEN_NAME(token_names, IDENTIFIER);
+	
+	ADD_TOKEN_NAME(token_names, LOGICAL_OP);
+	ADD_TOKEN_NAME(token_names,	BITWISE_OP);
+	ADD_TOKEN_NAME(token_names,	COMPARISON_OP);
+	ADD_TOKEN_NAME(token_names,	ASSIGNMENT_OP);
+	ADD_TOKEN_NAME(token_names,	ARITHMETIC_OP);
+	ADD_TOKEN_NAME(token_names,	BITSHIFT_OP);
+	
+	ADD_TOKEN_NAME(token_names, LEFT_PAREN);
+	ADD_TOKEN_NAME(token_names, RIGHT_PAREN);
+	ADD_TOKEN_NAME(token_names, LEFT_BRACKET);
+	ADD_TOKEN_NAME(token_names, RIGHT_BRACKET);
+	ADD_TOKEN_NAME(token_names, LEFT_CURLY_BRACE);
+	ADD_TOKEN_NAME(token_names, RIGHT_CURLY_BRACE);
+	ADD_TOKEN_NAME(token_names, COMMA);
+	ADD_TOKEN_NAME(token_names, SEMICOLON);
+	ADD_TOKEN_NAME(token_names, COLON);
 }
 
-// void get_token_name(char *name, int token_number) {
-// 	strcpy(name, token_names[token_number]);
-// }
-// 
+void initialize_operator_names() {
+	ADD_OPERATOR_NAME(operator_names, LOGICAL_NOT);
+	ADD_OPERATOR_NAME(operator_names, LOGICAL_OR);
+	ADD_OPERATOR_NAME(operator_names, LOGICAL_AND);
+	ADD_OPERATOR_NAME(operator_names, BITWISE_XOR);
+	ADD_OPERATOR_NAME(operator_names, BITWISE_AND);
+	ADD_OPERATOR_NAME(operator_names, BITWISE_OR);
+	ADD_OPERATOR_NAME(operator_names, BITWISE_COMPLEMENT);
+	ADD_OPERATOR_NAME(operator_names, IS_EQUAL);
+	ADD_OPERATOR_NAME(operator_names, IS_NOT_EQUAL);
+	ADD_OPERATOR_NAME(operator_names, LESS_THAN);
+	ADD_OPERATOR_NAME(operator_names, GREATER_THAN);
+	ADD_OPERATOR_NAME(operator_names, LESS_THAN_OR_EQUAL);
+	ADD_OPERATOR_NAME(operator_names, GREATER_THAN_OR_EQUAL);
+	ADD_OPERATOR_NAME(operator_names, ASSIGN);
+	ADD_OPERATOR_NAME(operator_names, ADD_AND_ASSIGN);
+	ADD_OPERATOR_NAME(operator_names, SUBTRACT_AND_ASSIGN);
+	ADD_OPERATOR_NAME(operator_names, MULTIPLY_AND_ASSIGN);
+	ADD_OPERATOR_NAME(operator_names, DIVIDE_AND_ASSIGN);
+	ADD_OPERATOR_NAME(operator_names, REMAINDER_AND_ASSIGN);
+	ADD_OPERATOR_NAME(operator_names, BITWISE_AND_AND_ASSIGN);
+	ADD_OPERATOR_NAME(operator_names, BITWISE_OR_AND_ASSIGN);
+	ADD_OPERATOR_NAME(operator_names, BITWISE_XOR_AND_ASSIGN);
+	ADD_OPERATOR_NAME(operator_names, BITSHIFT_LEFT_AND_ASSIGN);
+	ADD_OPERATOR_NAME(operator_names, BITSHIFT_RIGHT_AND_ASSIGN);
+	ADD_OPERATOR_NAME(operator_names, PLUS);
+	ADD_OPERATOR_NAME(operator_names, DASH);
+	ADD_OPERATOR_NAME(operator_names, MULTIPLY);
+	ADD_OPERATOR_NAME(operator_names, DIVIDE);
+	ADD_OPERATOR_NAME(operator_names, REMAINDER);
+	ADD_OPERATOR_NAME(operator_names, PREINCREMENT);
+	ADD_OPERATOR_NAME(operator_names, PREDECREMENT);
+	ADD_OPERATOR_NAME(operator_names, POSTINCREMENT);
+	ADD_OPERATOR_NAME(operator_names, POSTDECREMENT);
+	ADD_OPERATOR_NAME(operator_names, BITSHIFT_LEFT);
+	ADD_OPERATOR_NAME(operator_names, BITSHIFT_RIGHT);
+}
+
+void print_value(void *yylval) {
+	char *value = strdup((char *)yylval);
+	printf("Token value:\t%s\t", value);			
+	free(value);
+}
--- hw1/token_constants.h
+++ hw1/token_constants.h
@@ -1,28 +1,90 @@
 // reserved words
-#define	DO	1
-#define	FOR	2
-#define	RETURN	3
-#define	BREAK	4
-#define	SHORT	5
-#define	ELSE	6
-#define	GOTO	7
-#define	SIGNED	8
-#define	UNSIGNED	9
-#define	CHAR	10
-#define	IF	11
-#define	VOID	12
-#define	INT	13
-#define	CONTINUE	14
-#define	LONG	15
-#define	WHILE	16
-
+#define          	DO               	1
+#define          	FOR              	2
+#define          	RETURN           	3
+#define          	BREAK            	4
+#define          	SHORT            	5
+#define          	ELSE             	6
+#define          	GOTO             	7
+#define          	SIGNED           	8
+#define          	UNSIGNED         	9
+#define          	CHAR             	10
+#define          	IF               	11
+#define          	VOID             	12
+#define          	INT              	13
+#define          	CONTINUE         	14
+#define          	LONG             	15
+#define          	WHILE            	16
 //constants
-#define NUM_CONST 17
-#define STRING_CONST 18
-#define CHAR_CONST 19
+#define          	NUM_CONST        	17
+#define          	STRING_CONST     	18
+#define          	CHAR_CONST       	19
+//identifier
+#define          	IDENTIFIER       	20
+//operators
+#define          	LOGICAL_OP       	21
+#define          	BITWISE_OP       	22
+#define          	COMPARISON_OP    	23
+#define          	ASSIGNMENT_OP    	24
+#define          	ARITHMETIC_OP    	25
+#define          	BITSHIFT_OP      	26
+//separators
+#define          	LEFT_PAREN       	27
+#define          	RIGHT_PAREN      	28
+#define          	LEFT_BRACKET     	29
+#define          	RIGHT_BRACKET    	30
+#define          	LEFT_CURLY_BRACE 	31
+#define          	RIGHT_CURLY_BRACE	32
+#define          	COMMA            	33
+#define          	SEMICOLON        	34
+#define          	COLON            	35
 
 #define ADD_TOKEN_NAME(TOKEN_NAMES, TOKEN) TOKEN_NAMES[TOKEN] = #TOKEN
+#define ADD_OPERATOR_NAME(OPERATOR_NAMES, OP) OPERATOR_NAMES[OP] = #OP
+
 const char *token_names[100];
+const char *operator_names[100];
+
+#define	LOGICAL_NOT              	1
+#define	LOGICAL_OR               	2
+#define	LOGICAL_AND              	3
+
+#define	BITWISE_XOR              	5
+#define	BITWISE_AND              	6
+#define	BITWISE_OR               	7
+#define	BITWISE_COMPLEMENT       	8
+
+#define	IS_EQUAL                 	9
+#define	IS_NOT_EQUAL             	10
+#define	LESS_THAN                	11
+#define	GREATER_THAN             	12
+#define	LESS_THAN_OR_EQUAL       	13
+#define	GREATER_THAN_OR_EQUAL    	14
+
+#define	ASSIGN                   	15
+#define	ADD_AND_ASSIGN           	16
+#define	SUBTRACT_AND_ASSIGN      	17
+#define	MULTIPLY_AND_ASSIGN      	18
+#define	DIVIDE_AND_ASSIGN        	19
+#define	REMAINDER_AND_ASSIGN     	20
+#define	BITWISE_AND_AND_ASSIGN   	21
+#define	BITWISE_OR_AND_ASSIGN    	22
+#define	BITWISE_XOR_AND_ASSIGN   	23
+#define	BITSHIFT_LEFT_AND_ASSIGN 	24
+#define	BITSHIFT_RIGHT_AND_ASSIGN	25
+
+#define	PLUS                     	26
+#define	DASH                    	27
+#define	MULTIPLY                 	28
+#define	DIVIDE                   	29
+#define	REMAINDER                	30
+#define	PREINCREMENT             	31
+#define	PREDECREMENT             	32
+#define	POSTINCREMENT            	33
+#define	POSTDECREMENT            	34
+
+#define	BITSHIFT_LEFT            	36
+#define	BITSHIFT_RIGHT           	37
 
 struct number {
 	unsigned long value;
